700165ff8a522a822b9ab1ea2e056694deb0e329
YohSambre <YohSambre@users.noreply.github.com>
2024-10-01 12:04:30 +0200

    Add files via upload

---
+++ nuc_fuck_tool_contextmenu_hud/lua/autorun/client/nuc_fuck_tool_contextmenu_hud_cl.lua
@@ -0,0 +1,14 @@
+--------------------------------------------
+--*/ NUC JOIN THE BATTLE
+--------------------------------------------
+MsgC (Color(255, 255, 255), [[                                                                                                             
+= NUC - Fuck Tool ContextMenu Hud loaded!
+]])
+
+--------------------------------------------
+--*/ BASE
+--------------------------------------------
+local function Grrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr()
+    return false
+end
+hook.Add( "ContextMenuShowTool", "NUC_TOOL_CONTEXTMENU", Grrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr )
